edbe77219fcf043a5f90c8db141fd4c0b5650234
MWANGIKENNEDY <kenmwan75@gmail.com>
2024-08-03 18:29:27 +0300

    Created new card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="my-container lg:flex lg:space-x-10 space-y-8">
+    <main className="my-container md:flex md:space-x-10 space-y-8 pt-4">
 
       <Card/>
       <Card/>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div className="w-full inline-block text-white bg-gray-200 rounded-lg">
-      <div className="relative h-40">
+    <div className="w-full inline-block text-white bg-gray-200 rounded-lg overflow-hidden">
+      <div className="relative h-48">
         <Image
           src="https://images.unsplash.com/photo-1722189265463-557ef7d08d86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
@@ -14,11 +14,11 @@ const Card = () => {
         />
       </div>
 
-      <div className=" p-4 flex flex-col gap-4 ">
+      <div className=" p-4 flex flex-col gap-2 ">
 
-        <h3 className=" text-xl text-gray-900 font-bold">Card Title</h3>
+        <h3 className="  text-gray-900 font-bold">Card Title</h3>
 
-        <p className=" text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad fuga modi numquam odit doloribus autem unde ipsum minima sequi eligendi.</p>
+        <p className=" text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad fuga modi numquam odit doloribus autem unde ipsum minima sequi eligendi.</p>
 
         <button className="bg-green-500 px-6 py-3 rounded-md w-full">Click Me</button>
       </div>
