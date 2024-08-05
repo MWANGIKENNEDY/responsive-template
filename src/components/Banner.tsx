c460664df11b83536aeaa528db780583f30031a3
MWANGIKENNEDY <kenmwan75@gmail.com>
2024-08-05 15:55:48 +0300

    first commit

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <section className="min-h-[calc(100vh-5rem)] flex flex-col lg:flex-row-reverse lg:items-center  mx-auto  ">
-      <div className="h-1/3  md:h-1/2 lg:h-[50%]  lg:w-1/2 relative z-10">
+    <section className="min-h-[calc(100vh-5rem)] grid grid-rows-[30%,70%] md:grid-rows-[40%,60%] lg:grid-cols-2 lg:grid-rows-1  mx-auto lg:items-center ">
+      <div className=" h-full w-full lg:h-[50vh] relative z-10 bg-red-400 order-1 lg:order-2">
         <Image
           className=" h-full w-full object-cover "
           fill
           alt=""
-          src="https://images.unsplash.com/photo-1721932423849-e9033192b190?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1711486107626-855a7a5ca7f0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
         />
       </div>
-      <div className="flex h-2/3 flex-col gap-5 justify-center px-3 md:h-1/2 lg:h-full lg:gap-10 lg:w-1/2 lg:pr-20 ">
+      <div className="flex  py-3 flex-col gap-5 justify-center px-3  lg:pr-20 order-2 lg:order-1 ">
         <h1 className="text-3xl font-bold text-gray-800 lg:text-5xl capitalize">
           Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet.
         </h1>
